74053e945ce4dee98bdd17d01808902699396351
Ham144 <84296861+Ham144@users.noreply.github.com>
2024-01-09 21:52:36 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container} >
-      <Text >
-        awdad
-      </Text>
-    </SafeAreaView>
+    <ClerkProvider publishableKey='pk_test_bmVhcmJ5LWtpbmdmaXNoLTk2LmNsZXJrLmFjY291bnRzLmRldiQ'>
+      <View style={styles.container} >
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <Login />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   )
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: "#f2dcdc",
     alignItems: 'center',
     justifyContent: 'center',
   },
